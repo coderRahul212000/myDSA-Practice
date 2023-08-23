33e1f4135b33a30ef1f2f14f6985823054aa5475
Rahul Singh <136780822+coderRahul212000@users.noreply.github.com>
2023-08-23 09:16:59 +0530

    Update pattern18.cpp

--- lecture 4/pattern18.cpp
+++ lecture 4/pattern18.cpp
@@ -20,3 +20,39 @@ int main(){
     }
     return 0;
 }
+
+
+
+//2nd solution
+
+// Online C++ compiler to run C++ program online
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//             char val = 'D';
+//             char copy=val;
+//     for(int i = 1; i <= 4; i++){
+            
+//         for(int j = 1; j <= i; j++){
+//             cout<<copy++;
+                    
+           
+//         }
+        
+//         val--;
+//         copy = val;
+        
+        
+        
+
+
+            
+            
+        
+        
+
+//         cout<<endl;
+//     }
+//     return 0;
+// }
